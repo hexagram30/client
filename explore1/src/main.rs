05b76df48c9e4abfec32b9ebce43919da3c788db
Duncan McGreggor <oubiwann@gmail.com>
2020-02-08 11:52:29 -0600

    First demo.

---
+++ explore1/src/main.rs
@@ -0,0 +1,18 @@
+use rltk::{Rltk, GameState, Console};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build();
+    let gs = State{ };
+    rltk::main_loop(context, gs);
+}
